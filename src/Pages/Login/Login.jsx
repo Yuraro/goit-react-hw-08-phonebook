831190b332bed114246c4370d4e5cd63e635363b
Yuraro <romanykyriy@gmail.com>
2023-07-15 11:44:22 -0700

    Update

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,61 @@
+import { useDispatch } from 'react-redux';
+import css from './Login.module.css';
+import { useState } from 'react';
+import { login } from '../../redux/auth/auth-operations';
+
+const Login = () => {
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+        case 'email':
+            return setEmail(value);
+        case 'password':
+            return setPassword(value);
+        default:
+        return;
+    }
+};
+
+    const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(login({ email: email, password: password }));
+    setEmail('');
+    setPassword('');
+};
+
+    return (
+    <div className={css.form__container}>
+        <form autoComplete="off" className={css.form} onSubmit={handleSubmit}>
+        <label className={css.form__label}>
+            Email
+            <input
+            className={css.form__input}
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            ></input>
+        </label>
+
+        <label className={css.form__label}>
+            Password
+        <input
+            className={css.form__input}
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+        ></input>
+        </label>
+        <button type="submit" className={css.register__btn}>
+        LogIn
+        </button>
+        </form>
+    </div>
+);
+};
+
+export default Login;
---
+++ src/Pages/Registration/Registration.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import css from './Registration.module.css';
+import { useDispatch } from 'react-redux';
+import { register } from 'redux/auth/auth-operations';
+
+const Registration = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const dispatch = useDispatch();
+
+    const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+        case 'name':
+            return setName(value);
+        case 'email':
+            return setEmail(value);
+        case 'password':
+            return setPassword(value);
+        default:
+        return;
+    }
+    };
+
+    const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(register({ name: name, email: email, password: password }));
+    setName('');
+    setEmail('');
+    setPassword('');
+    };
+
+    return (
+    <div className={css.form__container}>
+        <form autoComplete="off" className={css.form} onSubmit={handleSubmit}>
+        <label className={css.form__label}>
+            Name
+        <input
+            className={css.form__input}
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChange}
+        ></input>
+        </label>
+        <label className={css.form__label}>
+            Email
+        <input
+            className={css.form__input}
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+        ></input>
+        </label>
+        <label className={css.form__label}>
+            Password
+        <input
+            className={css.form__input}
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+        ></input>
+        </label>
+        <button type="submit" className={css.register__btn}>
+            Sign up
+        </button>
+        </form>
+    </div>
+    );
+};
+
+export default Registration;
